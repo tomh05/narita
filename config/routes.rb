96a0ac160d4bbbeaa19b7b2660107d5370cf5e1e
Anthony Onumonu <onumoa01@139-80.ch.bbc.co.uk>
2015-11-19 09:41:39 +0000

    Change the order messages are imported

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,15 @@ Rails.application.routes.draw do
   resources :locations
   
   scope '/import' do
-    get 'locations', to: 'locations#import', :defaults => { :format => 'json' }
     get 'contacts', to: 'contacts#import', :defaults => { :format => 'json' }
     get 'calls', to: 'calls#import', :defaults => { :format => 'json' }
-    get 'sms', to: 'sms_messages#import', :defaults => { :format => 'json' }
     get 'apps', to: 'apps#import', :defaults => { :format => 'json' }
+
+    get 'sms', to: 'sms_messages#import', :defaults => { :format => 'json' }
+    get 'locations', to: 'locations#import', :defaults => { :format => 'json' }
+
+
   end
 
 end
+  
