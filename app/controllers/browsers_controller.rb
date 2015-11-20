2fd57dec8e53b86e13800f824a71222f22c54379
Anthony Onumonu <cisnky@gmail.com>
2015-11-20 13:41:59 +0000

    Fix: Update browsers form:

--- app/controllers/browsers_controller.rb
+++ app/controllers/browsers_controller.rb
@@ -62,9 +62,7 @@ class BrowsersController < ApplicationController
   end
 
   def import
-    # f = open("https://s3-us-west-2.amazonaws.com/bbcirfs/coot/logs/"+params[:filepath]+".csv");
-    f = open("https://s3-us-west-2.amazonaws.com/bbcirfs/coot/logs/cisnky_browser.csv");
-
+    f = open("https://s3-us-west-2.amazonaws.com/bbcirfs/coot/logs/"+params[:filepath]+".csv");
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
