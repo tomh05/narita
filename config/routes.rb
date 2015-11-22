26dfc1bc9ee0945872b221f98f77217a0b586760
Anthony Onumonu <cisnky@gmail.com>
2015-11-22 10:19:36 +0000

    Fix: Disable import

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,19 @@ Rails.application.routes.draw do
   resources :locations
   
   scope '/import' do
-    get 'contacts/:filepath', to: 'contacts#import', :defaults => { :format => 'json' }
-    get 'calls/:filepath', to: 'calls#import', :defaults => { :format => 'json' }
-    get 'apps/:filepath', to: 'apps#import', :defaults => { :format => 'json' }
-    get 'sms/:filepath', to: 'sms_messages#import', :defaults => { :format => 'json' }
-    get 'locations/:filepath', to: 'locations#import', :defaults => { :format => 'json' }
-    get 'browsers/:filepath', to: 'browsers#import', :defaults => { :format => 'json' }
+    # get 'contacts/:filepath', to: 'contacts#import', :defaults => { :format => 'json' }
+    # get 'calls/:filepath', to: 'calls#import', :defaults => { :format => 'json' }
+    # get 'apps/:filepath', to: 'apps#import', :defaults => { :format => 'json' }
+    # get 'sms/:filepath', to: 'sms_messages#import', :defaults => { :format => 'json' }
+    # get 'locations/:filepath', to: 'locations#import', :defaults => { :format => 'json' }
+    # get 'browsers/:filepath', to: 'browsers#import', :defaults => { :format => 'json' }
+    #
+    get 'contacts/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    get 'calls/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    get 'apps/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    get 'sms/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    get 'locations/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    get 'browsers/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
   end
   
   scope '/system' do
