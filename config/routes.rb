079dd5e180259a32d51d658c47f02ad45257f238
Anthony Onumonu <cisnky@gmail.com>
2015-11-25 22:10:01 +0000

    Feat: import photos that are taken

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :photos
+
   resources :browsers
 
   resources :sms_messages
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125153011) do
+ActiveRecord::Schema.define(version: 20151125210233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,12 @@ ActiveRecord::Schema.define(version: 20151125153011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "username"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sms_messages", force: :cascade do |t|
