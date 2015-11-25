1a59e2dc0a68c21aff65ec1c84cb8e71d0a6bd6f
Anthony Onumonu <cisnky@gmail.com>
2015-11-25 13:21:58 +0000

    Fix: Updated browser import script

--- app/controllers/browsers_controller.rb
+++ app/controllers/browsers_controller.rb
@@ -66,9 +66,10 @@ class BrowsersController < ApplicationController
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
-      parsed_event_time = DateTime.parse(row[0].to_s+'_'+row[1].to_s)
-      @browser = Browser.find_or_initialize_by(title: row[2].to_s, url: row[3].to_s, username: row[4].to_s)
-      @browser.event_time = parsed_event_time
+      # parsed_visit_date = DateTime.parse(row[0].to_s+'_'+row[1].to_s)
+      parsed_visit_date = DateTime.parse(row[4].to_s)
+      @browser = Browser.find_or_initialize_by(title: row[2].to_s, url: row[3].to_s, visit_total: row[5].to_i, username: row[6].to_s)
+      @browser.visit_date = parsed_visit_date
       @browser.save
     end
 
