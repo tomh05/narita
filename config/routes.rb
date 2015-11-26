362d04ba355c1eb65dc4e69ae9597c9b349e3803
Anthony Onumonu <cisnky@gmail.com>
2015-11-26 21:46:58 +0000

    Fix: Add back missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,20 +16,18 @@ Rails.application.routes.draw do
   
   scope '/import' do
     # get 'contacts/:filepath', to: 'contacts#import', :defaults => { :format => 'json' }
-    # get 'calls/:filepath', to: 'calls#import', :defaults => { :format => 'json' }
     # get 'apps/:filepath', to: 'apps#import', :defaults => { :format => 'json' }
 
-    # get 'locations/:filepath', to: 'locations#import', :defaults => { :format => 'json' }
+    get 'calls/:filepath', to: 'calls#import', :defaults => { :format => 'json' }
+    get 'locations/:filepath', to: 'locations#import', :defaults => { :format => 'json' }
     get 'browsers/:filepath', to: 'browsers#import', :defaults => { :format => 'json' }
     get 'sms/:filepath', to: 'sms_messages#import', :defaults => { :format => 'json' }
     get 'photos/:filename/:filetype/:username', to: 'photos#import', :defaults => { :format => 'json' }
     get 'appsummary/:filepath', to: 'apps#import_summary', :defaults => { :format => 'json' }
     #
-    get 'contacts/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
-    get 'calls/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
-
-    get 'locations/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
-
+    # get 'contacts/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    # get 'calls/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
+    # get 'locations/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
     # get 'apps/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
     # get 'sms/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
     # get 'browsers/:filepath', to: 'apps#ping', :defaults => { :format => 'json' }
