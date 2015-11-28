c37eb6ddd7c4d231c7aabec242343cd6731fe035
Anthony Onumonu <cisnky@gmail.com>
2015-11-28 10:35:07 +0000

    Fix: Don't set the date when creating a backup record. Just use rails default created attribute

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -66,7 +66,7 @@ class BackupsController < ApplicationController
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
-      @backup = Backup.find_or_initialize_by(started: DateTime.parse(row[2].to_s), username: row[3].to_s, ref: row[4].to_s)
+      @backup = Backup.find_or_initialize_by(username: row[3].to_s, ref: row[4].to_s)
       @backup.save
     end
 
