bd737712ba1f13ccddefb8eed265719837856023
Anthony Onumonu <cisnky@gmail.com>
2015-11-29 08:46:37 +0000

    Fix: Disable location import

--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,11 @@ Rails.application.routes.draw do
     post 'browsers', to: 'browsers#import', :defaults => { :format => 'json' }
     post 'appsummary', to: 'apps#import_summary', :defaults => { :format => 'json' }
     post 'calls', to: 'calls#import', :defaults => { :format => 'json' }
-    post 'locations', to: 'locations#import', :defaults => { :format => 'json' }
+    # post 'locations', to: 'locations#import', :defaults => { :format => 'json' }
     post 'photos', to: 'photos#import', :defaults => { :format => 'json' }
     post 'backup', to: 'backups#import', :defaults => { :format => 'json' }
 
+    post 'locations', to: 'apps#ping', :defaults => { :format => 'json' }
     # Not Used.
 
     # get 'contacts/:filepath', to: 'contacts#import', :defaults => { :format => 'json' }
