719b2f3ff311da391da1e7e9b658a7bc5d3d1099
Anthony Onumonu <cisnky@gmail.com>
2015-12-01 14:34:24 +0000

    Test: location report date format

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,7 +1,7 @@
 module LocationsHelper
 
     def format_date(current_date)
-      return current_date.strftime("#{%e.ordinalize} %b %Y, %H:%M")
+      return current_date.strftime("#{current_date.day.ordinalize} %b %Y, %H:%M")
       # return current_date.strftime("%e %b %Y, %H:%M")
     end
 
