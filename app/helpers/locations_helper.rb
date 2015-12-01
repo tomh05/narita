40f32d13c1884f056f8c7f8d2faa9cf2550be868
Anthony Onumonu <cisnky@gmail.com>
2015-12-01 16:13:56 +0000

    Fix: Calls report should return the date and the time

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,9 +1,5 @@
 module LocationsHelper
 
-    # def format_date current_date
-    #   return current_date.strftime("#{current_date.day.ordinalize} %b %Y, %H:%M")
-    # end
-
     def get_map_url lat, long
         return "https://www.google.com/maps?q="+lat+","+long
     end
