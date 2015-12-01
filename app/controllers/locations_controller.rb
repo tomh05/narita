c2ce25061f1a4fa3be3dce5749ac3581bb162107
Anthony Onumonu <cisnky@gmail.com>
2015-12-01 13:41:02 +0000

    Fix: Correct location event time

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -71,8 +71,8 @@ class LocationsController < ApplicationController
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
-      parsed_event_time = DateTime.parse(row[0].to_s+'_'+row[1].to_s)
-      @location = Location.find_or_initialize_by(event_time: parsed_event_time, lat: row[2].to_s, long: row[3].to_s, username: row[4].to_s)
+      parsed_event_time = DateTime.parse(row[4].to_s)
+      @location = Location.find_or_initialize_by(event_time: parsed_event_time, lat: row[2].to_s, long: row[3].to_s, username: row[5].to_s)
       @location.save
     end
 
