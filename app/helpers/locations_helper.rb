becae7aa27866c7e5ef54c33841b49e961966f79
Anthony Onumonu <cisnky@gmail.com>
2015-12-01 14:27:32 +0000

    Fix: Location report - format date

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,8 @@
 module LocationsHelper
+
+    def format_date(current_date)
+      # return current_date.strftime("{%e.ordinalize} %b %Y, %H:%M")
+      return current_date.strftime("%e %b %Y, %H:%M")
+    end
+
 end
