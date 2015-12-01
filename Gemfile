ea792016f5a13e4008378e732b9db4ac156480f9
Anthony Onumonu <cisnky@gmail.com>
2015-12-01 15:54:04 +0000

    Fix: Call update to include event time

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'rails_12factor'
 gem 'exifr'
+gem 'simplecov', :require => false, :group => :test
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -66,9 +66,7 @@ class CallsController < ApplicationController
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
-      parsed_event_time = DateTime.parse(row[0].to_s+'_'+row[1].to_s)
-      @contact = Call.find_or_initialize_by(call_date: row[2].to_s, call_number: row[3], call_type: row[4], call_duration: row[5], username: row[6].to_s)
-      @contact.event_time = parsed_event_time
+      @contact = Call.find_or_initialize_by(event_time: DateTime.parse(row[2].to_s), call_number: row[3], call_type: row[4], call_duration: row[5], username: row[6].to_s)
       @contact.save
     end
 
