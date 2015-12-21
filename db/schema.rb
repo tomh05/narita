6b5a828fe79d93d87110c3116218aeda358e9c76
Anthony Onumonu <anthonyo@139-80.ch.bbc.co.uk>
2015-12-21 14:29:14 +0000

    Lastest schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127064010) do
+ActiveRecord::Schema.define(version: 20151202085447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20151127064010) do
 
   create_table "calls", force: :cascade do |t|
     t.datetime "event_time"
-    t.date     "call_date"
     t.string   "call_number"
     t.string   "call_type"
     t.string   "call_duration"
