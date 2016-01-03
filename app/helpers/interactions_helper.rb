f1f5ef48fa8527abb611b87a928fb1a64a188c6f
tomh05 <tom.rr.howe@gmail.com>
2016-01-03 22:02:21 +0000

    interactions page

--- app/helpers/interactions_helper.rb
+++ app/helpers/interactions_helper.rb
@@ -1,2 +1,20 @@
 module InteractionsHelper
+    def roughtime(input_str)
+        input = Integer(input_str)
+        mm, ss = (input).divmod(60)            #=> [4515, 21]
+        hh, mm = mm.divmod(60)           #=> [75, 15]
+        dd, hh = hh.divmod(24)           #=> [3, 3]
+        if input < 60
+            return String(ss) +  " seconds"
+        elsif input < 3600
+            return String(mm) + " minutes"
+        elsif input < 3600*2
+            return String(hh) + " hour " +  String(mm) + " minutes"
+        elsif input < 3600*24
+            return String(hh) + " hours " +  String(mm) + " minutes"
+        else
+            return String(dd) + " days " + String(hh)  + " hours " +  String(mm) + " minutes"
+        end
+
+    end
 end
---
+++ app/models/interaction.rb
@@ -0,0 +1,8 @@
+class Interaction
+    attr_accessor :type, :datetime, :content, :duration, :direction, :missed
+    #def initialize(type,name)
+    #    @type = type
+    #    @name = name
+    #end
+
+end
