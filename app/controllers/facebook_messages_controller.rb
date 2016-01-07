02cc0db9d7fe1663cb5dbf049b215c1aca84b451
Anthony Onumonu <cisnky@gmail.com>
2016-01-07 10:44:54 +0000

    Fix: Import script. Date was requested from wrong column

--- app/controllers/facebook_messages_controller.rb
+++ app/controllers/facebook_messages_controller.rb
@@ -68,11 +68,9 @@ class FacebookMessagesController < ApplicationController
     csv_text = f.read
     csv = CSV.parse(csv_text, :headers => false)
     csv.each do |row|
-      parsed_message_date = DateTime.parse(row[6].to_s)
-      @fb = FacebookMessage.find_or_initialize_by(fb_from: row[2].to_s, fb_content: URI.unescape(row[7].to_s), username: row[4].to_s)
+      parsed_message_date = DateTime.parse(row[4].to_s)
+      @fb = FacebookMessage.find_or_initialize_by(fb_from: row[2].to_s, fb_content: URI.unescape(row[3].to_s), username: row[5].to_s)
       @fb.fb_date = parsed_message_date
-      @fb.white_list = row[5].to_s
-      @fb.sms_folder = row[3].to_s
       @fb.save
     end
 
